bb585e17a3d69ee06319ab93fac463039fb8ffc7
eduardo gomez <ergomez@storecheck.com>
2025-02-09 18:05:24 -0600

    implement borrow records and finish the app

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,11 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import React from 'react'
 import Link from 'next/link'
-import {cn, getInitials} from "@/lib/utils"
 import Image from "next/image"
-import { Session } from 'next-auth'
+import { signOut } from '@/auth'
+import { Button } from './ui/button'
 
-const Header = ({session}: {session: Session}) => {
-    const pathname = usePathname();
+const Header = () => {
+
   return (
     <header className='my-10 flex justify-between gap-5'>
         <Link href="/">
@@ -16,25 +13,16 @@ const Header = ({session}: {session: Session}) => {
         </Link>
 
         <ul className='flex flex-row items-center gap-8'>
+            
             <li>
-                <Link
-                    href="/library"
-                    className={cn(
-                        "text-base cursor-pointer capitalize",
-                        pathname === '/library' ? 'text-light-200' : "text-light-100",
-                    )}
-                >
-                    Library
-                </Link>
-            </li>
-            <li>
-                <Link href="/my-profile">
-                    <Avatar>
-                        <AvatarFallback className='bg-amber-100'>
-                            {getInitials(session?.user?.name || "IN")}
-                        </AvatarFallback>
-                    </Avatar>
-                </Link>
+                <form action={async () => {
+                    "use server";
+
+                    await signOut();
+                }} 
+                className='mb-10'>
+                  <Button>Logout</Button>
+                </form>
             </li>
         </ul>
     </header>
--- database/schema.ts
+++ database/schema.ts
@@ -1,3 +1,6 @@
+import { auth } from "@/auth";
+import { create } from "domain";
+import { desc } from "drizzle-orm";
 import { integer, text, boolean, pgTable, uuid, varchar, pgEnum, date, timestamp } from "drizzle-orm/pg-core";
 
 export const STATUS_ENUM = pgEnum("status", ['PENDING', 'APPROVED', 'REJECTED']);
@@ -17,4 +20,31 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at", {
     withTimezone: true,
   }).defaultNow(),
+});
+
+export const books = pgTable("books", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  title: varchar("title", { length: 255 }).notNull(),
+  author: varchar("author", { length: 255 }).notNull(),
+  genre: text("genre").notNull(),
+  rating: integer("rating").notNull(),
+  coverUrl: text("cover_url").notNull(),
+  coverColor: varchar("cover_color", {length: 7}).notNull(),
+  description: text("description").notNull(),
+  totalCopies: integer("total_copies").notNull().default(1),
+  availableCopies: integer("available_copies").notNull().default(0),
+  videoUrl: text("video_url").notNull(),
+  summary: varchar("summary").notNull(),
+  createdAt: timestamp("created_at", {withTimezone: true}).defaultNow(),
+});
+
+export const borrowRecords = pgTable("borrow_records", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  bookId: uuid("book_id").notNull().references(() => books.id),
+  userId: uuid("user_id").notNull().references(() => users.id),
+  borrowDate: timestamp("borrow_date", {withTimezone: true}).defaultNow().notNull(),
+  dueDate: date("due_date").notNull(),
+  returnDate: date("return_date"),
+  status: BORROW_STATUS_ENUM("status").default("BORROWED").notNull(),
+  createdAt: timestamp("created_at", {withTimezone: true}).defaultNow(),
 });
